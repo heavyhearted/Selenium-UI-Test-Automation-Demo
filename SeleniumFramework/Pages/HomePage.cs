4067f4d73759bdf9c52720e09ab4b9d28141e4e0
Dessy Dimitrova <desislava.d@icloud.com>
2024-11-11 13:27:04 +0200

    Move Header and Footer section common methods to the Base page

--- SeleniumFramework/Pages/HomePage.cs
+++ SeleniumFramework/Pages/HomePage.cs
@@ -9,8 +9,6 @@ namespace SeleniumFramework.Pages
     public class HomePage : BasePage
     {
         private readonly HomePageLocators _locators;  
-        private readonly HeaderSection _headerSection;
-        private readonly FooterSection _footerSection;
         
         public const string PageTitle = "Push Higher Quality Software To Market Faster";
 
@@ -23,8 +21,6 @@ namespace SeleniumFramework.Pages
             : base(driver)
         {
             _locators = locators;
-            _headerSection = new HeaderSection(driver); 
-            _footerSection = new FooterSection(driver);
         }
         
         public override void Open()
@@ -32,24 +28,10 @@ namespace SeleniumFramework.Pages
             Driver.Navigate().GoToUrl(WebAppTestUrls.HomePageUrl); 
         }
         
-        public string GetMainTitleText()
+        public string GetPageTitle()
         {
             var titleElement = Wait.Until(ExpectedConditions.ElementIsVisible(_locators.MainTitle));
             return titleElement.Text;
         }
-
-        public bool IsHeaderLogoVisible() => _headerSection.IsHeaderLogoVisible();
-        
-        public bool IsFooterLogoVisible() => _footerSection.IsFooterLogoVisible();
-        
-        public string GetCopyrightText() => _footerSection.GetCopyrightText(); 
-        
-        public bool IsTermsAndConditionsVisible() => _footerSection.IsTermsAndConditionsVisible();
-    
-        public void ClickTermsAndConditions() => _footerSection.ClickTermsAndConditions();
-    
-        public bool IsPrivacyPolicyVisible() => _footerSection.IsPrivacyPolicyVisible();
-    
-        public void ClickPrivacyPolicy() => _footerSection.ClickPrivacyPolicy();
     }
 }
