ad516f71229e170ce5ae1e457926ae82cb0224c6
Dessy Dimitrova <desislava.d@icloud.com>
2024-11-11 19:24:10 +0200

    Add new GenerateSearchUrl method to the WebAppUrlValidationHelper to validate redirection from the Blog page

--- SeleniumFramework/CoreFramework/Utilities/WebAppUrlValidationHelper.cs
+++ SeleniumFramework/CoreFramework/Utilities/WebAppUrlValidationHelper.cs
@@ -3,12 +3,18 @@ using SeleniumFramework.CoreFramework.Models;
 
 namespace SeleniumFramework.CoreFramework.Utilities;
 
-public static class ApplicationFormUrlHelper
+public static class WebAppUrlValidationHelper
 {
     public static string GenerateApplicationFormSubmissionUrl(TestUser testUser)
     {
         var encodedFirstName = HttpUtility.UrlEncode(testUser.FirstName);
         var encodedLastName = HttpUtility.UrlEncode(testUser.LastName);
         return $"https://ultimateqa.com/?gender={testUser.Gender.ToString().ToLower()}&firstname={encodedFirstName}&lastname={encodedLastName}";
+    }
+    
+    public static string GenerateSearchUrl(string searchInput)
+    {
+        var encodedSearchInput = HttpUtility.UrlEncode(searchInput);
+        return $"https://ultimateqa.com/?s={encodedSearchInput}&et_pb_searchform_submit=et_search_proccess&et_pb_include_posts=yes&et_pb_include_pages=yes";
     }
 }
